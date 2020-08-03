918f7ee06745beff2bd5887829e37b4cc875c513
aryclenio <aryclenio2010@gmail.com>
2020-08-03 19:43:23 -0300

    :rocket: Finishing Landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing/Index';
+
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
